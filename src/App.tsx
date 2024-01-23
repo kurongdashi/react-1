2ad2a46545ce622d90c8af1ec063413442b18774
lp123 <1970664102@qq.com>
2024-01-23 16:30:17 +0800

    feat:父子应用通信

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import './public-path'
+// 引入环境变量
+import './public-path';
 import React, { Suspense } from 'react';
-import ReactDOM,{ createRoot } from 'react-dom/client';
+import ReactDOM, { createRoot } from 'react-dom/client';
 import Entry from './Entry';
 import './App.less';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
@@ -15,6 +16,7 @@ import zhCN from 'antd/locale/zh_CN';
 import dayjs from 'dayjs';
 import 'dayjs/locale/zh-cn';
 dayjs.locale('zh-cn');
+import { runAfterFirstMounted } from 'qiankun';
 // 可合并reducer
 // const reduceMerge=combineReducers(reducer);
 // 使用applyMiddleware 中间件，可以支持action 返回一个方法
@@ -35,13 +37,20 @@ const App = () => {
     </ConfigProvider>
   );
 };
-const render=(props?:any)=>{
-  debugger
-  const root = createRoot(props.container ? props.container.querySelector('#root') : document.getElementById('root'));
+const render = (props?: any) => {
+  debugger;
+  const root = createRoot(
+    props.container
+      ? props.container.querySelector('#root')
+      : document.getElementById('root')
+  );
   root.render(<App />);
-}
+};
 // 非乾坤还是走原来的逻辑
-console.log('(window as any).__POWERED_BY_QIANKUN__',(window as any).__POWERED_BY_QIANKUN__);
+console.log(
+  '(window as any).__POWERED_BY_QIANKUN__',
+  (window as any).__POWERED_BY_QIANKUN__
+);
 if (!(window as any).__POWERED_BY_QIANKUN__) {
   render({});
 }
@@ -56,16 +65,29 @@ export async function bootstrap() {
 /**
  * 应用每次进入都会调用 mount 方法，通常我们在这里触发应用的渲染方法
  */
-export async function mount(props:any) {
-  console.log('react app mount');
+export async function mount(props: any) {
+  const obj = props.singleSpa.getAppStatus();
+  console.log('react app1 mount', props, obj);
+  props.onGlobalStateChange((state, prev) => {
+    console.log('子应用1监听state', state, prev);
+    render(props);
+  });
   render(props);
 }
 
 /**
  * 应用每次 切出/卸载 会调用的方法，通常在这里我们会卸载微应用的应用实例
  */
-export async function unmount(props:any) {
+export async function unmount(props: any) {
   console.log('react app unmount');
-  const root = createRoot(props.container ? props.container.querySelector('#root') : document.getElementById('root'));
+  const root = createRoot(
+    props.container
+      ? props.container.querySelector('#root')
+      : document.getElementById('root')
+  );
   root.unmount();
 }
+// 首个子应用加载后，可执行回调
+runAfterFirstMounted(() => {
+  console.log('第一个子应用启动了~');
+});
